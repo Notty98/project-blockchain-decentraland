7ae138d39a9f364084a9eee8a17f1c6ae7623325
Stefano <stefano.notari1998@gmail.com>
2023-01-19 15:48:37 +0100

    updated tuning's parameter

--- tuning.py
+++ tuning.py
@@ -1,13 +1,14 @@
 import xgboost as xgb
 from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.multioutput import MultiOutputRegressor
+from sklearn.metrics import mean_squared_error, make_scorer, r2_score
 import pandas as pd
 import numpy as np 
 
 raw_data = pd.read_csv('./DCL_summary.csv')
 
 # create a matrix with only the column that have a positive correlation with the price (see analysis.py)
-X = raw_data.loc[:, ['transactions','ETH','ETH_7d','COIN_7d', 'tweet_meta', 'tweet_pojo', 'x','y','estate_size','traffic_cum_sum']]
+X = raw_data.loc[:, ['transactions', 'ETH','ETH_7d','COIN', 'COIN_7d', 'google', 'tweet_meta', 'tweet_pojo']]
 y = raw_data.loc[:, ['price_usd']] # Target variable
 
 
@@ -37,26 +38,29 @@ dtrain = xgb.DMatrix(data = X, label=y)
 
 domain_params_dicts = {
     #reg:linear deprecated
-    'objective' : ['reg:squarederror','reg:squaredlogerror','reg:gamma','reg:tweedie'],
+    'objective' : ['reg:squaredlogerror','reg:gamma','reg:tweedie', 'reg:squarederror', 'reg:logistic', 'reg:poisson', 'reg:quantile'],
 
-    'n_estimators': [50, 100, 150],
+    'n_estimators': [50, 250, 500],
 
-    'max_depth': [3, 5, 9],
+    'max_depth': [25, 100, 250],
 
     'min_child_weight': [0.001, 0.1, 1],
 
-    'gamma': [0.01, 0.1, 0.5],
+    'gamma': [0.001, 0.01, 0.5],
 
     'subsample': [i/10 for i in range(4,10)],
 
-    'learning_rate' : [0.5, 0.25 , 0.05]
+    'learning_rate' : [0.5, 0.05 , 0.005]
 
 }
+
+#mean_squared_error = make_scorer(mean_squared_error, greater_is_better=False)
+r2 = make_scorer(r2_score)
 
 gsc = GridSearchCV(
             estimator=xgb.XGBRegressor(),
             param_grid=domain_params_dicts,
-            cv=3, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+            cv=3, scoring=['neg_mean_squared_error', 'r2'], refit='r2', verbose=0, n_jobs=-1)
 
 grid_result = MultiOutputRegressor(gsc).fit(X_train, y_train)
 best_params = grid_result.estimators_[0].best_params_
