17348b99a62efce49cffe6e3c0d5b31827891f50
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-02-06 16:16:21 +0100

    update: added comment and removed estate_size from the analysis

--- sperman.py
+++ sperman.py
@@ -5,13 +5,17 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('./DCL_summary.csv')
 
-df.drop('is_road',inplace=True,axis=1)
-df.drop('is_plaza',inplace=True,axis=1)
-df.drop('x',inplace=True,axis=1)
-df.drop('y',inplace=True,axis=1)
+# remove the features that cannot be used in the correlation analysis
+df.drop('is_road', inplace=True, axis=1)
+df.drop('is_plaza', inplace=True, axis=1)
+df.drop('estate_size', inplace=True, axis=1)
+df.drop('x', inplace=True, axis=1)
+df.drop('y', inplace=True, axis=1)
 
+# create the spearmann correlation matrix
 corr_spearman = df.corr(method='spearman')
 
-sns.heatmap(corr_spearman, xticklabels=corr_spearman.columns, yticklabels=corr_spearman.columns, cmap='RdBu')
+# create a heatmap with the features name and the reversed color (Red with high correlation and Blue with low correlation)
+sns.heatmap(corr_spearman, xticklabels=corr_spearman.columns, yticklabels=corr_spearman.columns, cmap='RdBu_r')
 
 plt.show()
