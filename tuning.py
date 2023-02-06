c4df3dedb135cd0552137909b84147be7ab756da
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-02-06 16:37:36 +0100

    update: added comments

--- tuning.py
+++ tuning.py
@@ -1,48 +1,29 @@
 import xgboost as xgb
 from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.multioutput import MultiOutputRegressor
-from sklearn.metrics import mean_squared_error, make_scorer, r2_score
+from sklearn.metrics import make_scorer, r2_score
 import pandas as pd
-import numpy as np 
 
+# load the dataset
 raw_data = pd.read_csv('./DCL_summary.csv')
 
-# create a matrix with only the column that have a positive correlation with the price (see analysis.py)
-X = raw_data.loc[:, ['transactions', 'ETH','ETH_7d','COIN', 'COIN_7d', 'google', 'tweet_meta', 'tweet_pojo']]
-y = raw_data.loc[:, ['price_usd']] # Target variable
+# create the set of features (X) that will be used to approximate the price_usd (y)
+X = raw_data.loc[:, ['transactions','x','y','is_road','is_plaza','estate_size','traffic_cum_sum','traffic_max','traffic_avg','ETH','ETH_7d','COIN','COIN_7d','google','tweet_meta','tweet_pojo']]
+y = raw_data.loc[:, ['price_usd']]
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 dtrain = xgb.DMatrix(data = X, label=y)
 
-""" domain_params_dicts = {
-    #reg:linear deprecated
-    'objective' : ['reg:squarederror','reg:squaredlogerror','reg:gamma','reg:tweedie'],
-
-    'n_estimators': [30, 50, 70, 100, 150, 200, 300],
-
-    'max_depth': [3, 5, 7, 9],
-
-    'min_child_weight': [0.001, 0.1, 1, 5, 10, 20],
-
-    'gamma': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5],
-
-    'n_estimators': [30, 50, 70, 100, 150, 200, 300],
-
-    'subsample': [i/10 for i in range(4,10)],
-
-    'learning_rate' : [0.5, 0.25 , 0.05, 0.005]
-
-} """
-
+# define the values of parameters that need to be tuned
 domain_params_dicts = {
     #reg:linear deprecated
-    'objective' : ['reg:squaredlogerror','reg:gamma','reg:tweedie', 'reg:squarederror', 'reg:logistic', 'reg:poisson', 'reg:quantile'],
+    'objective' : ['reg:squaredlogerror','reg:gamma','reg:tweedie', 'reg:squarederror', 'count:poisson'],
 
     'n_estimators': [50, 250, 500],
 
-    'max_depth': [25, 100, 250],
+    'max_depth': [25, 100, 250, 500],
 
     'min_child_weight': [0.001, 0.1, 1],
 
@@ -54,16 +35,22 @@ domain_params_dicts = {
 
 }
 
-#mean_squared_error = make_scorer(mean_squared_error, greater_is_better=False)
+# define the accuracy
 r2 = make_scorer(r2_score)
 
+# define the SearchCV to obtain the best value that have the best accuracy and the least loss
 gsc = GridSearchCV(
             estimator=xgb.XGBRegressor(),
             param_grid=domain_params_dicts,
-            cv=3, scoring=['neg_mean_squared_error', 'r2'], refit='r2', verbose=0, n_jobs=-1)
+            cv=3, 
+            scoring=['neg_mean_squared_error', 'r2'], 
+            refit='r2', 
+            verbose=0, 
+            n_jobs=-1)
 
 grid_result = MultiOutputRegressor(gsc).fit(X_train, y_train)
 best_params = grid_result.estimators_[0].best_params_
+
 print(grid_result.estimators_)
 print(best_params)
 
