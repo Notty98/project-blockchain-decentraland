6939966b135f537a75788bb3129d64c1140437e2
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-02-08 16:57:27 +0100

    added models with tuned parameters

--- model_subset.py
+++ model_subset.py
@@ -12,14 +12,19 @@ import matplotlib.pyplot as plt
 
 raw_data = pd.read_csv('./DCL_summary.csv')
 
-# create a matrix with the relevant features of the dataset
+#remove the row that doesn't have traffic
+for index, row in raw_data.iterrows():
+    if row['traffic_avg'] == 0:
+        raw_data.drop(index, axis=0, inplace=True)
+
+# create a matrix with the relevant features
 X = raw_data.loc[:, ['transactions','x','y','is_road','is_plaza','estate_size','traffic_cum_sum','traffic_max','traffic_avg','ETH','ETH_7d','COIN','COIN_7d','google','tweet_meta','tweet_pojo']]
-y = raw_data.loc[:, ['price_usd']]
+y = raw_data.loc[:, ['price_usd']] # Target variable
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # XGBoost model
-model = xgb.XGBRegressor(gamma= 0.5, learning_rate= 0.05, max_depth= 25, min_child_weight= 1, n_estimators= 500, objective = 'reg:gamma', subsample= 0.9)
+model = xgb.XGBRegressor(gamma= 0.01, learning_rate= 0.05, max_depth= 25, min_child_weight= 1, n_estimators= 500, objective = 'reg:gamma', subsample= 0.4)
 model.fit(X_train, y_train)
 
 importance = model.get_booster().get_score(importance_type='weight')
@@ -42,14 +47,14 @@ print("MAE : % f" %(mae))
 r2 = r2_score(y_test, y_pred)
 print("R2 : % f" %(r2))
 
-# Create a scatter plot of the predictions and the true values
+# Create a scatter plot of the approximation and the true values
 plt.scatter(y_test, y_pred)
 
 # Add a line for perfect prediction
 plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=4)
 
-plt.xlabel('True Values(USD)')
-plt.ylabel('Predictions(USD)')
-plt.title('XGBoost Predictions')
+plt.xlabel('True Values (USD)')
+plt.ylabel('Approximation (USD)')
+plt.title('XGBoost Approximation')
 
 plt.show()
